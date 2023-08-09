2ac1b6ee4c6daa5842812ccf84f028ad6c846109
Stef Rouschop <stef@rouschop.com>
2023-08-09 09:05:52 +0200

    Refactor PreferredLanguageTest.php

--- tests/Feature/Services/PreferredLanguageTest.php
+++ tests/Feature/Services/PreferredLanguageTest.php
@@ -4,7 +4,6 @@
 use Stefro\LaravelLangCountry\Services\PreferredLanguage;
 
 beforeEach(function () {
-    // Set config variables
     $this->app['config']->set('lang-country.fallback', 'en-GB');
     $this->app['config']->set('lang-country.allowed', [
         'nl-NL',
@@ -17,7 +16,7 @@ beforeEach(function () {
     ]);
 });
 
-it('get preferred languages in right order', function () {
+it('should get preferred languages in right order', function () {
     $lang = new PreferredLanguage('nl-NL,nl;q=0.8,en-US;q=0.6,en;q=0.4,de;q=0.5');
 
     $expected = collect([
@@ -29,7 +28,7 @@ it('get preferred languages in right order', function () {
     ]);
 
     expect($lang->clientPreferedLanguages())->toEqual($expected);
-})->group('preferred_lang_test');
+});
 
 it('prioritize more specific values', function () {
     $lang = new PreferredLanguage('nl-NL,nl,en-US,en,de');
@@ -42,43 +41,43 @@ it('prioritize more specific values', function () {
     ]);
 
     expect($lang->clientPreferedLanguages())->toEqual($expected);
-})->group('preferred_lang_test');
+});
 
-it('exact match with allowed lang country', function () {
+it('should return exact match with allowed lang country', function () {
     $lang = new PreferredLanguage('gr,nl,de-DE,en');
 
     expect($lang->lang_country)->toEqual('de-DE');
-})->group('preferred_lang_test');
+});
 
-it('match based on lang', function () {
+it('should match based on lang', function () {
     $lang = new PreferredLanguage('gr,nl,zh-CH,en');
 
     expect($lang->lang_country)->toEqual('nl-NL');
-})->group('preferred_lang_test');
+});
 
-it('no match return fallback', function () {
+it('should return fallback when there is no match', function () {
     $lang = new PreferredLanguage('gr,zh-CH');
 
     expect($lang->lang_country)->toEqual('en-GB');
-})->group('preferred_lang_test');
+});
 
-it('country in lowecase', function () {
+it('should return country in lowercase', function () {
     $lang = new PreferredLanguage('nl-nl,nl-be,gr');
 
     expect($lang->lang_country)->toEqual('nl-NL');
-})->group('preferred_lang_test');
+});
 
-it('will override the default allowed languages from the config', function () {
+it('should override the default allowed languages from the config', function () {
     $override_allowed_languages = collect([
         'es-ES', 'es-CO', 'no-NO', 'cn-CN',
     ]);
     $override_fallback = 'cn-CN';
 
     $lang = new PreferredLanguage('es', $override_allowed_languages, $override_fallback);
-    expect($lang->lang_country)->toEqual('es-ES');
-    expect($lang->locale)->toEqual('es');
+    expect($lang->lang_country)->toEqual('es-ES')
+        ->and($lang->locale)->toEqual('es');
 
     $lang = new PreferredLanguage('nl', $override_allowed_languages, $override_fallback);
-    expect($lang->lang_country)->toEqual('cn-CN');
-    expect($lang->locale)->toEqual('cn');
+    expect($lang->lang_country)->toEqual('cn-CN')
+        ->and($lang->locale)->toEqual('cn');
 });
