4d49139d580b2069ba2f48e9d88b2a9b84313404
Stef Rouschop <stef@rouschop.com>
2024-06-21 18:21:33 +0200

    wip

---
+++ src/.stubs.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Carbon;
+
+/**
+ * These stubs are used to provide IDE autocomplete for the LaravelLangCountry package.
+ */
+{
+    /**
+     * @method string langCountryDateNumbers(bool|string $override = false, bool $withTime = false)
+     * @method string langCountryDateWordsWithoutDay(bool|string $override = false, bool $withTime = false)
+     * @method string langCountryDateWordsWithDay(bool|string $override = false, bool $withTime = false)
+     * @method string langCountryDateBirthday(bool|string $override = false)
+     * @method string langCountryTime(bool|string $override = false)
+     */
+    class Carbon
+    {
+    }
+}
